f78b59a9876169dd80a4e01f2e84e18c694bdcaa
Ajay Aujla <ajay.work@hotmail.com>
2020-05-01 13:30:24 -0400

    Initial

---
+++ src/main/java/com/appdirect/kafkapocmaven/KafkaPocMavenApplication.java
@@ -0,0 +1,13 @@
+package com.appdirect.kafkapocmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPocMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPocMavenApplication.class, args);
+    }
+
+}
